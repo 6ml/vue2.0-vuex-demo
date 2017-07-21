c7590cab9d3d0d4ced81684bd7a536dcfc177ec3
lupath <849104822@qq.com>
2017-07-21 16:12:54 +0800

    [fix] upload username and avatar

--- src/components/LogTime.vue
+++ src/components/LogTime.vue
@@ -1,11 +1,15 @@
 <template>
 	<div class="form-horizontal">
 		<div class="form-group">
-			<div class="col-sm-6">
+			<div class="col-sm-4">
+				<label>昵称</label>
+				<input type="text" class="form-control" v-model="username" placeholder="UserName">
+			</div>
+			<div class="col-sm-4">
 				<label>日期</label>
 				<input type="date" class="form-control" v-model="date" placeholder="Date">
 			</div>
-			<div class="col-sm-6">
+			<div class="col-sm-4">
 				<label>时间</label>
 				<input type="number" class="form-control" v-model="totalTime" placeholder="Hours">
 			</div>
@@ -14,6 +18,12 @@
 			<div class="col-sm-12">
 				<label>备注</label>
 				<input type="text" class="form-control" v-model="comment" placeholder="Comment">
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-12">
+				<label>头像</label>
+				<input type="file" class="file" accept="image/png,image/gif,image/jpeg" @change="update">
 			</div>
 		</div>
 		<button class="btn btn-primary" @click="save()">保存</button>
@@ -26,23 +36,31 @@
 		name: "LogTime",
 		data () {
 			return {
+				username: '',
 				date: '',
 				totalTime: '',
-				comment: ''
+				comment: '',
+				plan: {}
 			}
 		},
 		methods: {
 			save () {
-				const plan = {
-					username: 'lupath',
-					avatar: 'avatar',
-					date : this.date,
-					totalTime : parseInt(this.totalTime),
-					comment : this.comment
-				};
-				this.$store.dispatch('savePlan', plan)
-				// this.$store.dispatch('addTotalTime', this.totalTime)
+				if(!this.plan.avatar){
+					alert('请先选择头像!');
+				}
+				else{
+					this.plan.username = this.username;
+					this.plan.date = this.date;
+					this.plan.totalTime = this.totalTime;
+					this.plan.comment = this.comment;
+
+					this.$store.dispatch('savePlan', this.plan)
+				}
 				this.$router.go(-1)
+			},
+			update (e) {
+				let file = e.target.files[0];
+				this.plan.avatar = file;
 			}
 		}
 	}
